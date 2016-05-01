3da17f3eb0466f3340ca786873d589699f22fa3d
Faisal Waris <faisalwaris@live.com>
2016-05-01 13:07:49 -0400

    initial

---
+++ CALib/Testing/TestCA3D.fsx
@@ -0,0 +1,59 @@
+﻿#r @"../../packages/FSharp.Collections.ParallelSeq.1.0.2\lib\net40\FSharp.Collections.ParallelSeq.dll"
+#load "../CA.fs"
+#load "../CAUtils.fs"
+#load "../BeliefSpace/SituationalKS.fs"
+#load "../BeliefSpace/NormativeKS.fs"
+#load "../BeliefSpace/HistoricalKS.fs"
+#load "../BeliefSpace/DomainKS.fs"
+#load "../CARunner.fs"
+open CA
+
+let parms = 
+    [|
+        F(0.,-0.,10.) // x
+        F(0.,-0.,10.) // y
+        F(0.,-0.,10.) // z
+    |]
+
+//http://en.wikipedia.org/wiki/Nonlinear_programming
+// maximize xy + yz
+// st. x^2 - y^2 + z^2 <= 2
+//     x^2 + y^2 + z^2 <= 10
+let fitness (parms:Parm array) = 
+    let x = match parms.[0] with F(v,_,_) -> v | _ -> failwith "no match"
+    let y = match parms.[1] with F(v,_,_) -> v | _ -> failwith "no match"
+    let z = match parms.[2] with F(v,_,_) -> v | _ -> failwith "no match"
+    let c1 = x**2. - y**2. + z**2. <= 2.
+    let c2 = x**2. + y**2. + z**2. <= 10.
+    if c1 && c2 then
+        x*y + y*z
+    else
+        -9999.0
+
+let comparator  = CAUtils.Maximize
+let beliefSpace = CARunner.defaultBeliefSpace parms comparator fitness
+//let beliefSpace = Leaf (SituationalKS.create comparator 5)
+let pop         = CAUtils.createPop parms 1000 beliefSpace true
+
+let ca =
+    {
+        Population           = pop
+        Network              = CAUtils.l4BestNetwork
+        KnowlegeDistribution = CARunner.knowledgeDistribution CARunner.majority
+        BeliefSpace          = beliefSpace
+        Acceptance           = CARunner.acceptance 5 comparator
+        Influence            = CARunner.influence
+        Update               = CARunner.update
+        Fitness              = fitness
+        Comparator           = comparator
+    }
+
+let termination step = step.Count > 1000
+
+(*
+let r = (CARunner.run  termination 2 ca)
+r.Best
+r.CA.Population |> Seq.countBy (fun p -> p.KS)
+r.CA.Fitness r.Best.[0].Parms
+*)
+
---
+++ CALib/Testing/TestParms.fsx
@@ -0,0 +1,32 @@
+﻿#load "../CA.fs"
+#load "../CAUtils.fs"
+
+open CA
+open CAUtils
+let rec dummyKS = {Type=Situational; Accept=(fun xs -> xs,dummyKS); Influence=fun i -> i}
+let testParms =
+    [|
+        F(10.,10.,100.)
+        F32(10.f,10.f,100.f)
+        I(10,10,100)
+        I64(10L,10L,100L)
+    |]
+
+let ind0 = {Id=0; Parms=testParms; Fitness=0.; KS=Situational}
+let ind1 = {Id=1; Parms=testParms |> Array.map randomize; Fitness=0.; KS=Situational}
+
+let evolved = ind1.Parms |> Array.map evolveS
+if (evolved,ind1.Parms) ||> Array.exists2 (fun a b -> a = b) then
+    failwith "evolveS"
+
+let ind01 = influenceInd ind0 ind1
+if (ind0.Parms,ind01.Parms) ||> Array.exists2 (fun a b -> a > b) then
+    failwith "influenceInd"
+
+let ind1_up = ind1.Parms |> Array.map slideUp
+if (ind1.Parms,ind1_up) ||> Array.exists2 (fun a b -> a > b) then
+    failwith "slideUp"
+
+let ind1_down = ind1.Parms |> Array.map slideDown
+if (ind1.Parms,ind1_down) ||> Array.exists2 (fun a b -> a < b) then
+    failwith "slideDown"
